6ba61f4f81ee72d5be2f30e2f387405e57f7cf26
ana <yosip19h@gmail.com>
2025-05-09 18:35:20 +0500

    aasd222

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -75,7 +75,7 @@ const ContactForm = () => {
         console.log('Отправляемые данные:', payload);
 
         try {
-            const response = await fetch('/api/lead', {
+            const response = await fetch('https://Dake2025.pythonanywhere.com/lead', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -86,6 +86,7 @@ const ContactForm = () => {
             if (response.ok) {
                 console.log('Заявка успешно отправлена:', payload);
                 setError('');
+                onClose();
             } else {
                 const contentType = response.headers.get('content-type');
                 if (contentType && contentType.includes('application/json')) {
