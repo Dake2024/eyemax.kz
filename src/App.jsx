5b637a8dbd9df70eab58e74ff4b2049e36cf551d
Dake2024 <110532707+Dake2024@users.noreply.github.com>
2025-05-15 22:59:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,81 @@
-import './App.css';
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import LandingPage from "./pages/LandingPage.jsx";
-import {useEffect, useState} from "react";
-import { Routes, Route } from "react-router-dom";
-import Modal from "./components/Modal.jsx";
+import React, { useEffect } from 'react'; // Добавляем useEffect
+import Hero from "../components/Hero.jsx";
+import Banners from "../components/Banners.jsx";
+import Steps from "../components/Steps.jsx";
+import Request from "../components/Request.jsx";
+import Reviews from "../components/Reviews.jsx";
+import Announcement from "../components/Announcment.jsx";
+import FAG from "../components/FAG.jsx";
+import Contact from "../components/Contact.jsx";
 
-function App() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isScrolled, setIsScrolled] = useState(false);
-
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
+const LandingPage = ({ onOpenModal }) => {
+  // Добавляем эффект для инициализации пикселя
+  useEffect(() => {
+    // Проверяем загрузился ли пиксель
+    const initPixel = () => {
+      if (typeof fbq === 'function') {
+        fbq('init', '1157267149417924');
+        fbq('track', 'PageView');
+      } else {
+        // Если не загрузился - проверяем снова через 500мс
+        setTimeout(initPixel, 500);
+      }
     };
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
+    // Первоначальная инициализация
+    if (!window.fbq) {
+      window.fbq = function() {
+        window.fbq.callMethod ?
+        window.fbq.callMethod.apply(window.fbq, arguments) :
+        window.fbq.queue.push(arguments);
+      };
+      window.fbq.push = window.fbq;
+      window.fbq.loaded = true;
+      window.fbq.version = '2.0';
+      window.fbq.queue = [];
+      
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://connect.facebook.net/en_US/fbevents.js';
+      document.head.appendChild(script);
+    }
+
+    initPixel();
+
+    // Очистка при размонтировании (опционально)
+    return () => {
+      if (typeof fbq === 'function') {
+        fbq('track', 'PageView');
+      }
     };
+  }, []); // Пустой массив - выполняется только при монтировании
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsScrolled(window.scrollY > 10);
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+  return (
+    <>
+      <Hero onOpenModal={onOpenModal}/>
+      <Banners onOpenModal={onOpenModal}/>
+      <Steps onOpenModal={onOpenModal}/>
+      <div className="w-full flex justify-center">
+        <Request onOpenModal={onOpenModal}/>
+      </div>
+      <Reviews onOpenModal={onOpenModal}/>
+      <Announcement onOpenModal={onOpenModal}/>
+      <div className="w-full flex justify-center">
+        <FAG onOpenModal={onOpenModal}/>
+      </div>
+      <Contact onOpenModal={onOpenModal}/>
 
-    return (
-        <div className="flex flex-col min-h-screen bg-[#F9F9F9]">
-            {/* Передаем handleOpenModal в Header */}
-            <div className={`sticky top-0 z-50 transition-all duration-50 bg-transparent`}>
-                <div className="md:px-[6%] md:py-[3%]">
-                    <Header onOpenModal={handleOpenModal} />
-                </div>
-            </div>
+      {/* Добавляем noscript-тэг */}
+      <noscript>
+        <img
+          height="1"
+          width="1" 
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1157267149417924&ev=PageView&noscript=1"
+        />
+      </noscript>
+    </>
+  );
+};
 
-            <main className="flex-grow z-1">
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <LandingPage onOpenModal={handleOpenModal} />
-                        }
-                    />
-                    <Route
-                        path="/request"
-                        element={
-                            <LandingPage onOpenModal={handleOpenModal} />
-                        }
-                    />
-                </Routes>
-            </main>
-
-            <Footer />
-
-            {/* Модальное окно */}
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
-        </div>
-    );
-}
-
-export default App;
+export default LandingPage;
